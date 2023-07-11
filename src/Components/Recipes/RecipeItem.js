5fc21a44e09078a9b224964b8d0b76eddda59551
HAlGhanim <imzaji98@gmail.com>
2023-07-11 19:59:55 +0300

    integration 1

--- src/Components/Recipes/RecipeItem.js
+++ src/Components/Recipes/RecipeItem.js
@@ -3,16 +3,20 @@ import React from "react";
 const RecipeItem = ({ recipe }) => {
   return (
     <div className="col-md-6 col-lg-4 mb-5">
-      <Link to={`/recipe-details/${recipe.slug}`}>
-        <div className="mx-auto">
-          <div className="d-flex align-items-center justify-content-center h-100 w-100">
-            <div className="text-center text-white">
-              <i className="fas fa-plus fa-3x"></i>
+      <ul className="flex flex-wrap justify-center gap-10">
+        <li key={recipe.id} className="py-2">
+          <div className="flex flex-col items-center border border-gray-300 bg-white rounded-lg h-[500px] w-[200px] xl:w-[500px] p-4 hover:scale-105 transition-transform duration-300">
+            <div className="h-64 w-full mb-4">
+              <img
+                src={`http://localhost:8000/${recipe.image}`}
+                alt={recipe.name}
+                className="h-full w-full object-cover rounded-lg"
+              />
             </div>
+            <div className="text-center">{recipe.name}</div>
           </div>
-          <img className="img-fluid" src={recipe.image} alt="Recipe" /> 
-        </div>
-      </Link>
+        </li>
+      </ul>
     </div>
   );
 };
