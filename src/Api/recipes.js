f8b7246b7a8bcb09004fd1e4c5f5d4bf6da4c1fd
HAlGhanim <imzaji98@gmail.com>
2023-07-11 21:30:52 +0300

    temp

--- src/Api/recipes.js
+++ src/Api/recipes.js
@@ -1,7 +1,10 @@
 import instance from ".";
 
-const createRecipe = async () => {
-  const { data } = await instance.post("/recipes");
+const createRecipe = async (recipeInfo) => {
+  console.log(recipeInfo);
+  const formData = new FormData();
+  for (const key in recipeInfo) formData.append(key, recipeInfo[key]);
+  const { data } = await instance.post("/recipes", formData);
   return data;
 };
 
--- src/Components/Recipes/Input.js
+++ src/Components/Recipes/Input.js
@@ -1,11 +1,38 @@
 import React from "react";
 
-const Input = ({ name, onChange }) => {
+const Input = ({ name, type, onChange }) => {
+  const handleLabelClick = (e) => {
+    e.stopPropagation();
+  };
+
+  if (type === "file") {
+    return (
+      <div className="flex justify-start ml-3">
+        <h1 className="w-[25%]">{name}</h1>
+        <label
+          onClick={handleLabelClick}
+        >
+          <input
+            name={name}
+            type={type}
+            onChange={onChange}
+            className="hidden"
+            required
+          />
+          <span className="px-2 py-1 bg-gray-200 cursor-pointer rounded-md">
+            Select a file
+          </span>
+        </label>
+      </div>
+    );
+  }
+
   return (
-    <div className="flex justify-center ">
+    <div className="flex justify-center">
       <h1 className="w-[25%]">{name}</h1>
       <input
         name={name}
+        type={type}
         onChange={onChange}
         className="w-[70%] border pl-2 border-black rounded-md"
         placeholder={name}
